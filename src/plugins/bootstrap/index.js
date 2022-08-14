38f1c609ed222caafe468e363b32df2071a3309a
Raihan Kabir <25036102+rk4bir@users.noreply.github.com>
2022-08-14 14:53:52 +0000

    Update index.js

--- src/plugins/bootstrap/index.js
+++ src/plugins/bootstrap/index.js
@@ -1,2 +1,11 @@
 import "./app.scss"
 import * as bootstrap from 'bootstrap'
+
+
+// initialise tooltips
+document.onreadystatechange = function () {
+    if (document.readyState === 'complete') {
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    }
+};
